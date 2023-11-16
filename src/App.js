bfe03a1b68578c9700b592594c2351c2eca719ec
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2023-11-16 16:17:39 +0100

    main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,62 @@
+import { useState } from "react"
+import NewProject from "./components/NewProject"
+import NoProjectSelected from "./components/NoProjectSelected"
+import ProjectSideBar from "./components/ProjectSideBar"
+
 const App = () => {
+  const [projectState, setProjectState] =useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  const handleStartAddProject = () => {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    })
+  };
+ 
+  const handleCancelAddProject = () => {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined
+        }
+        })
+  };
+
+  const handleAddProject = (projectData) => {
+    setProjectState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  console.log(projectState);
+
+  let content;
+  if (projectState.selectedProjectId === null) {
+    content= <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+  } else if (projectState.selectedProjectId===undefined) {
+    content = <NoProjectSelected onAddProject={handleStartAddProject}/>
+  }
+
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <main className="h-screen my-8 flex gap-8">
+        <ProjectSideBar onAddProject={handleStartAddProject} projects={projectState.projects}/>
+       {content}
+        
+    </main>
     </>
   )
 }
